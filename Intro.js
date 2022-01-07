191faf3f8a361ca07a5bf5eac8a7bae4fb36c0db
P7112540 <P7112540@INAIRLT541133.ntil.com>
2022-01-06 18:03:01 -0800

    Functions & Parameters

---
+++ Intro.js
@@ -0,0 +1,15 @@
+function add(){   // Default function i.e no parameter
+ var a= 100;
+ var b = 200;
+ var c = a+b;
+ console.log(c);
+}
+add();
+
+function Mul(x,y){   // Parameterised function i.e with parameter
+    var a = x
+    var b = y
+    var c = a * b;
+    console.log(c);
+   }
+   Mul(22,33);
