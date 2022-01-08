2fec8719e92e147bfa7a850c2e63fca09229c775
P7112540 <P7112540@INAIRLT541133.ntil.com>
2022-01-07 19:20:45 -0800

    array concept

---
+++ array.js
@@ -0,0 +1,12 @@
+var info = [
+    "sagar",
+    "kabir",
+    "KS@gmail.com",
+    989890,
+    { city: "Mumbai", state: "MH" },
+    [90, 87, { pincode: 898989 }],
+  ];
+
+  console.log(info[0]);
+  console.log(info[4].state);
+  console.log(info[5][2].pincode);
